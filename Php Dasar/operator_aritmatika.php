77eaa65f84cb1cba2488145b484765cae77f0ff8
yanasaputra18 <yanasaputra152@gmail.com>
2026-08-05 00:42:40 +0700

    opratoor aritmatika

---
+++ Php Dasar/operator_aritmatika.php
@@ -0,0 +1,26 @@
+<?php
+
+// $a = 100;
+// $b = 20;
+
+// echo $a - $b; // Output: 80
+// echo "\n";
+// echo $a + $b; // Output: 120
+// echo "\n";
+// echo $a * $b; // Output: 2000
+// echo "\n";
+// echo $a / $b; // Output: 5
+// echo "\n";
+// echo $a % 30; // Output: 10
+
+$angka1 = 25;
+$angka2 = 4;
+
+echo "==== OPERATOR ARITMATIKA ====\n";
+echo "Angka 1 : $angka1\n";
+echo "Angka 2 : $angka2\n";
+echo "Hasil Penjumlahan : " . ($angka1 + $angka2) . "\n";
+echo "Hasil Pengurangan : " . ($angka1 - $angka2) . "\n";
+echo "Hasil Perkalian : " . ($angka1 * $angka2) . "\n";
+echo "Hasil Pembagian : " . ($angka1 / $angka2) . "\n";
+echo "Hasil Modulus : " . ($angka1 % $angka2) . "\n";

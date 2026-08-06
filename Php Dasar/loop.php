89bf0d81b02057484bbd9140ada80f64a7226acd
yanasaputra18 <yanasaputra152@gmail.com>
2026-08-06 13:13:32 +0700

    materi loop selesai

--- Php Dasar/loop.php
+++ Php Dasar/loop.php
@@ -60,8 +60,90 @@ for ($i = 1; $i <= 3; $i++) {
 
 
 // latihan 8 loop dalam kolom
-for ($i = 1; $i <= 3; $i++) {
-    for ($j = 1; $j <= 2; $j++) {
-        echo "Baris $i Kolom $j \n";
+for ($i = 1; $i <= 1; $i++) {
+    for ($j = 2; $j <= 2; $j++) {
+        for ($k = 3; $k <= 3; $k++) {
+            echo "$i,$j,$k \n";
+            echo "$i,$j,$k \n";
+            echo "$i,$j,$k \n";
+        }
     }
 }   
+
+
+// latihan 9 loop bersarang bintang
+for ($i = 1; $i <= 5; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "\n";
+}
+
+
+// latihan 10 loop menggunakan while
+$i = 1;
+while ($i <= 5) {
+    echo "Perulangan ke-$i \n";
+    $i++;
+}
+
+
+// latihan 11 loop menggunakan while datanya mulai dari 10
+$i = 10;
+while ($i >= 1) {
+    echo "ke-$i \n";
+    $i--;
+}
+
+
+// latihan 12 loop menggunakan while dengan increment 2
+$i = 2;
+while ($i <= 10) {
+    echo "pilihan ganda ke-$i \n";
+    $i += 2;
+}
+
+
+// berikan contoh loop while dengan decrement 10
+
+$i = 100;
+
+while ($i > 0) {
+
+    echo $i . "\n";
+
+    $i -= 10;
+
+}
+
+
+
+// latihan 13 loop menggunakan do while
+$i = 1;
+do {
+    echo "Perulangan ke-$i \n";
+    $i++;
+} while ($i <= 5);  
+
+
+// latihan 14 loop menggunakan do while dengan decrement
+
+$i = 10;
+do{
+    echo "ke-$i \n";
+    $i--;
+}while ($i >= 1);
+
+
+// latihan 15 loop menggunakan do while dengan increment 2
+
+
+$i = 50;
+
+do {
+
+    echo $i . "\n";
+
+    $i += 10;
+
+} while ($i <= 40);

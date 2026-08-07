4e2505286f50d957eee5e937ebfed4cd2e565667
yanasaputra18 <yanasaputra152@gmail.com>
2026-08-07 18:50:08 +0700

    update

---
+++ Php Dasar/array.php
@@ -0,0 +1,32 @@
+<?php
+// latihan 1 array
+$hewan = ["kucing", "anjing", "kelinci", "kambing", "sapi"];
+echo "Hewan pertama adalah " . $hewan[0] . "\n";
+echo "Hewan kedua adalah " . $hewan[1] . "\n";
+echo "Hewan ketiga adalah " . $hewan[2] . "\n";
+echo "Hewan keempat adalah " . $hewan[3] . "\n";
+echo "Hewan kelima adalah " . $hewan[4] . "\n";
+
+
+
+// latihan 2 Buat array kosong.
+$angka[] = 10;
+$angka[] = 20;
+$angka[] = 30;
+$angka[] = 40;
+$angka[] = 50;
+echo "Angka pertama adalah " . $angka[0] . "\n";
+echo "Angka kedua adalah " . $angka[1] . "\n";
+echo "Angka ketiga adalah " . $angka[2] . "\n";
+echo "Angka keempat adalah " . $angka[3] . "\n";
+echo "Angka kelima adalah " . $angka[4] . "\n";
+
+
+// latihan 3 Buat array mengubah warna
+$warna = ["merah", "hijau", "hijau"];
+$warna[1] = "kuning";
+
+echo $warna[0] . "\n";
+echo $warna[1] . "\n";
+echo $warna[2] . "\n"; 
+    

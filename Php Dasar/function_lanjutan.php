163d963a5dcc9b3899747a5a429b9d317cd887c9
yanasaputra18 <yanasaputra152@gmail.com>
2026-08-12 23:49:07 +0700

    update

--- Php Dasar/function_lanjutan.php
+++ Php Dasar/function_lanjutan.php
@@ -187,3 +187,205 @@ echo cekUmur(null) . "\n";
 // NEXT: ARRAY SEBAGAI PARAMETER & RETURN
 
 // belajar 12
+function tampilkanBuah(array $buah): void
+{
+    foreach ($buah as $key => $value) {
+        echo "Buah ke-" . $key + 1 . " adalah " . $value . "\n";
+    }
+}
+$buah = ["Mangga", "Apel", "Jeruk", "Semangka"];
+tampilkanBuah($buah);
+// output:
+// Buah ke-1 adalah Mangga
+// Buah ke-2 adalah Apel
+// Buah ke-3 adalah Jeruk
+// Buah ke-4 adalah Semangka
+
+
+
+// belajar 13
+function getNamaMahasiswa(): array
+{
+    return ["Yana", "Asep", "Siti"];
+}
+
+$mahasiswa = getNamaMahasiswa();
+
+foreach ($mahasiswa as $key => $value) {
+    echo "Mahasiswa ke-" . $key + 1 . " adalah " . $value . "\n";
+}
+// output
+// Mahasiswa ke-1 adalah Yana
+// Mahasiswa ke-2 adalah Asep
+// Mahasiswa ke-3 adalah Siti
+
+
+
+
+// belajar 14
+function hitungTotal(array $harga): int
+{
+    return array_sum($harga);
+}
+
+$total = 0;
+$harga = [10000, 20000, 30000];
+foreach ($harga as $key => $value) {
+    $total += $value;
+}
+echo $total . "\n";
+// output 60000     
+
+
+// Apa arti array $data pada parameter function? = array $data adalah parameter yang menerima nilai array
+// Apa arti : array setelah nama function? = mengembalikan nilai array
+// Apa perbedaan function getData(): array dan function getData(array $data) = function getData(): array adalah function yang mengembalikan nilai array, function getData(array $data) adalah function yang menerima nilai array
+// Menurutmu, kenapa function yang mengembalikan array akan berguna dalam aplikasi nyata? = function yang mengembalikan array akan berguna dalam aplikasi nyata, karena function yang mengembalikan array akan berguna dalam aplikasi nyata
+
+
+
+
+// belajar 15
+function ambilNilaiLulus(array $nilai): array
+{
+    $lulus = [];
+    foreach($nilai as $key => $value) {
+        if ($value >= 60) {
+            $lulus[] = $value;
+        }
+    }
+    return $lulus;
+}
+
+$nilai = [80, 60, 50, 70, 90];
+$lulus = ambilNilaiLulus($nilai);
+echo implode(", ", $lulus) . "\n";
+// output 80, 60, 70, 90
+
+
+
+// belajar 16
+$produk = [
+    [
+        "nama" => "Laptop",
+        "harga" => 7000000,
+        "jumlah" => 2
+    ],
+    [
+        "nama" => "Mouse",
+        "harga" => 150000,
+        "jumlah" => 3
+    ],
+    [
+        "nama" => "Keyboard",
+        "harga" => 300000,
+        "jumlah" => 2
+    ]
+];
+    function hitungTotalBelanja(array $produk): int
+{
+    $total = 0;
+    foreach ($produk as $key => $value) {
+        $total += $value['harga'] * $value['jumlah'];
+    }
+    return $total;
+}
+
+echo "Total harga: " . hitungTotalBelanja($produk) . "\n";
+// output Total harga: 15050000
+
+
+
+// belajar 17
+$harga = [10000, 20000, 15000, 5000];
+function hitungTotal2(array $harga): int
+{
+    $total = 0;
+    foreach($harga as $key => $value) {
+        $total += $value;
+    }
+    return $total;
+}
+
+echo "Total harga : " .  hitungTotal2($harga) . "\n";
+// output Total harga : 50000
+
+
+
+// 5 KEYWORD PENTING
+/*
+1. Type Declaration String, Int, Float, Bool, Array, Null
+2. Return Type
+3. Variable arguments
+4. Callback
+5. Closure
+*/
+
+
+
+
+
+// Next belajar Filter Nilai
+
+// belajar 18
+function filterNilai(array $nilai): array
+{
+    $lulus = [];
+    foreach($nilai as $key => $value) {
+        if ($value >= 60) {
+            $lulus[] = $value;
+        }
+    }
+    return $lulus;
+}
+
+$nilai = [80, 60, 50, 70, 90];
+$lulus = filterNilai($nilai);
+echo implode(", ", $lulus) . "\n";
+// output 80, 60, 70, 90
+
+
+// belajar 19
+$produk = [
+    [
+        "nama" => "Laptop",
+        "harga" => 7000000,
+        "stok" => 5
+    ],
+    [
+        "nama" => "Mouse",
+        "harga" => 150000,
+        "stok" => 0
+    ],
+    [
+        "nama" => "Keyboard",
+        "harga" => 300000,
+        "stok" => 10
+    ],
+    [
+        "nama" => "Monitor",
+        "harga" => 2000000,
+        "stok" => 0
+    ]
+];
+function produkTersedia(array $produk): array
+{
+    $tersedia = [];
+    foreach($produk as $key => $value) {
+        if ($value['stok'] > 0) {
+            $tersedia[] = $value;
+        }
+    }
+    return $tersedia;
+}
+
+$produkTersedia = produkTersedia($produk);
+echo "Produk tersedia: " . count($produkTersedia) . "\n";
+// output Produk tersedia: 2
+
+
+// pertanyaan 
+// 1. kenapa kita membuat $hasil = []; sebelum di forech 
+// jawaban = untuk menampung nilai yang akan di return
+// 2. Apa fungsi if ($value >= 60) dalam Mini Challenge 18? = untuk memfilter nilai yang akan di return
+// 3. 
